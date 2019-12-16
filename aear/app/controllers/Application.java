e4462ab4ab89414379ee80f6646bd17d849faa49
Antonio McMichael <antonio_mcmichael@hotmail.com>
2019-12-15 18:32:51 -0800

    Adding initial content and design.

---
+++ aear/app/controllers/Application.java
@@ -0,0 +1,36 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class Application extends Controller
+{
+    public static void index()
+	{
+        render();
+    }
+	
+	public static void services()
+	{
+        render();
+    }
+	
+	public static void about()
+	{
+        render();
+    }
+	
+	public static void contact()
+	{
+        render();
+    }
+	
+	public static void contactPost()
+	{
+        render();
+    }
+}
